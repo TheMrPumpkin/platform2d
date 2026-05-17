6acdd536b869da5ed500c8f4269e147c2993b69e
TheMrPumpkin <bobtttyr@gmail.com>
2026-05-17 14:21:05 +0300

    fix enemy jump

--- src/main.c
+++ src/main.c
@@ -17,17 +17,17 @@ Player player = {
     .isGrounded = true,
     .velocity = 0.0f,
     .HP = 3};
-
 Enemy enemy[5];
 
 const float gravity = 0.6f;
 Platform platforms[] = {
-    {.box = {.x = 400, .y = 450, .width = ScreenWidth, .height = ScreenHeight / 12}},
+    {.box = {.x = 400, .y = 450, .width = ScreenWidth / 1, .height = ScreenHeight / 12}},
     {.box = {.x = 400, .y = 250, .width = ScreenWidth / 8, .height = ScreenHeight / 2}}};
 
 int main(void)
 {
     enemy[0] = enemy_main(910.0f, 0.0f, 100.0f, 100.0f, 2.0f, 100.0f);
+    bool is_running = true;
 
     Camera2D camera = {0};
     camera.target = (Vector2){player.box.x + 20.0f, player.box.y + 20.0f};
@@ -36,33 +36,47 @@ int main(void)
     camera.zoom = 1.0f;
 
     int platform_size = sizeof(platforms) / sizeof(platforms[0]);
-    int enemy_size = sizeof(enemy) / sizeof(enemy[0]);
-
-    player.box.x = 100;
+    int enemy_size = sizeof(enemy[0]) / sizeof(enemy[0]);
+    player.box.x = (ScreenWidth - player.box.width) / 1;
     InitWindow(ScreenWidth, ScreenHeight, "GAME");
 
     Texture2D texplayer = LoadTexture("Textures/Texpumpkin.png");
     Texture2D texwall = LoadTexture("Textures/Texwall.png");
     Texture2D texenemy = LoadTexture("Textures/Texapple.png");
 
+    float mid_W = ScreenWidth / 2;
+    float mid_H = ScreenHeight / 2;
+
     float cooldownDuration = 1.2f;
     float cooldownTimer = 0.0f;
-
+    bool isColliding = false;
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
+
         if (cooldownTimer > 0.0f)
+        {
             cooldownTimer -= GetFrameTime();
+        }
 
         for (int i = 0; i < platform_size; i++)
+        {
             platform_update(&platforms[i]);
+        }
 
+        for (int i = 0; i < enemy_size; i++)
+        {
+            if (player.HP > 0)
+            {
+                player_update(&player);
+                enemy_update(&enemy[i]);
+                is_running = false;
+            }
+        }
         if (player.HP > 0)
         {
             player_update(&player);
-            for (int i = 0; i < enemy_size; i++)
-                enemy_update(&enemy[i]);
+            is_running = false;
         }
 
         for (int i = 0; i < platform_size; i++)
@@ -73,6 +87,20 @@ int main(void)
                     player.box.x = platforms[i].box.x - player.box.width;
                 if (IsKeyDown(KEY_LEFT))
                     player.box.x = platforms[i].box.x + platforms[i].box.width;
+            }
+        }
+        for (int i = 0; i < enemy_size; i++)
+        {
+            if (CheckCollisionRecs(player.box, enemy[i].box))
+            {
+                if (cooldownTimer <= 0.0f)
+                {
+                    if (player.box.width == enemy->box.width && player.box.height == enemy->box.height)
+                    {
+                        player.HP -= 1;
+                    }
+                    cooldownTimer = cooldownDuration;
+                }
             }
         }
 
@@ -98,20 +126,24 @@ int main(void)
             }
         }
 
-        for (int i = 0; i < enemy_size; i++)
-        {
-            enemy[i].velocity.y += gravity;
-            enemy[i].box.y += enemy[i].velocity.y;
+        enemy->velocity.y += gravity;
+        enemy->box.y += enemy->velocity.y;
+        enemy->isGrounded = false;
 
-            for (int j = 0; j < platform_size; j++)
+        for (int i = 0; i < platform_size; i++)
+        {
+            if (CheckCollisionRecs(enemy->box, platforms[i].box))
             {
-                if (CheckCollisionRecs(enemy[i].box, platforms[j].box))
+                if (enemy->velocity.y > 0)
                 {
-                    if (enemy[i].velocity.y > 0)
-                    {
-                        enemy[i].box.y = platforms[j].box.y - enemy[i].box.height;
-                        enemy[i].velocity.y = 0.0f;
-                    }
+                    enemy->box.y = platforms[i].box.y - enemy->box.height;
+                    enemy->velocity.y = 0.0f;
+                    enemy->isGrounded = true;
+                }
+                else if (player.velocity.y < 0)
+                {
+                    player.box.y = platforms[i].box.y + platforms[i].box.height;
+                    player.velocity.y = 0.0f;
                 }
             }
         }
@@ -120,56 +152,83 @@ int main(void)
         {
             if (CheckCollisionRecs(player.box, enemy[i].box))
             {
-                if (player.velocity.y > 0 && (player.box.y + player.box.height) < (enemy[i].box.y + 20))
+                if ((player.box.y + player.box.height) < (enemy[i].box.y + 20))
                 {
                     player.velocity.y = -12.0f;
                     player.box.y = enemy[i].box.y - player.box.height;
                 }
-                else if (cooldownTimer <= 0.0f)
+            }
+        }
+        for (int i = 0; i < platform_size; i++)
+        {
+            if (CheckCollisionRecs(player.box, platforms[i].box))
+            {
+                if (player.velocity.y > 0)
                 {
-                    player.HP -= 1;
-                    cooldownTimer = cooldownDuration;
+                    player.box.y = platforms[i].box.y - player.box.height;
+                    player.velocity.y = 0.0f;
+                    player.isGrounded = true;
+                }
+                else if (player.velocity.y < 0)
+                {
+                    player.box.y = platforms[i].box.y + platforms[i].box.height;
+                    player.velocity.y = 0.0f;
                 }
             }
         }
 
-        if (player.HP <= 0 && IsKeyPressed(KEY_R))
+        for (int i = 0; i < enemy_size; i++)
         {
-            player.HP = 3;
-            player.box.x = 100;
-            player.box.y = 100;
-            player.velocity.y = 0;
+            if (player.HP <= 0)
+            {
+                if (IsKeyPressed(KEY_R))
+                {
+                    player.HP = 3;
+                    player.box = (Rectangle){700.0, 350.0, 100.0, 100.0};
+                    enemy[i].box = (Rectangle){910.0, 0.0, 100.0, 100.0};
+                };
+            }
         }
 
         camera.target = (Vector2){player.box.x + 20, player.box.y + 20};
 
         BeginDrawing();
-        ClearBackground(GRAY);
         DrawFPS(10, 10);
-
+        ClearBackground(GRAY);
         BeginMode2D(camera);
         player_render(&player, texplayer);
-        for (int i = 0; i < enemy_size; i++)
-            enemy_render(&enemy[i], texenemy);
-        for (int i = 0; i < platform_size; i++)
-            platform_render(&platforms[i], texwall);
-        EndMode2D();
 
+        for (int i = 0; i < enemy_size; i++)
+        {
+            // printf("X | %f , Y | %f\n", enemy->box.x, enemy->box.y);
+            enemy_render(&enemy[i], texenemy);
+        }
+
+        for (int i = 0; i < platform_size; i++)
+        {
+            platform_render(&platforms[i], texwall);
+        }
+
+        EndMode2D();
         if (player.HP <= 0)
         {
-            DrawText("GAME OVER!", 250, 200, 50, RED);
-            DrawText("PRESS R TO CONTINUE!", 100, 250, 50, BLACK);
+            player.HP = 0;
+            DrawText(TextFormat("AHAHAHAHAHA!!!!", player.HP), 20, 400, 50, BLACK);
         }
         else
         {
-            DrawText(TextFormat("HP: %d", player.HP), 20, 400, 50, BLACK);
+            DrawText(TextFormat("HP %d", player.HP), 20, 400, 50, BLACK);
+        }
+
+        if (player.HP <= 0)
+        {
+            DrawText(TextFormat("GAME OVER!"), 250, 200, 50, RED);
+            DrawText(TextFormat("PRESS R TO CONTINUE!"), 100, 250, 50, BLACK);
         }
         EndDrawing();
     }
-
     UnloadTexture(texplayer);
     UnloadTexture(texwall);
-    UnloadTexture(texenemy);
     CloseWindow();
 
     return 0;
